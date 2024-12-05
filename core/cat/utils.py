c0f5d8a3bc30f4919c0f30399d9651ea64b4b0e5
davidebizzocchi <davidebizzocchi19@gmail.com>
2024-12-05 16:00:36 +0100

    custom singleton

--- core/cat/utils.py
+++ core/cat/utils.py
@@ -254,6 +254,16 @@ class singleton:
             return cls.instances[class_]
 
         return getinstance
+    
+class singleton_meta(type):
+    """A metaclass to implement the Singleton pattern."""
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            # If instance doesn't exist, create and store it
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
 
 
 # Class mixing pydantic BaseModel with dictionaries (added for backward compatibility, to be deprecated in v2)
