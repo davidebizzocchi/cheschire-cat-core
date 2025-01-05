01ce7429ff4a62e9821051b8b959ddac539c65ec
davidebizzocchi <davidebizzocchi19@gmail.com>
2025-01-06 00:28:55 +0100

    option decorator examples

--- core/cat/mad_hatter/core_plugin/options.py
+++ core/cat/mad_hatter/core_plugin/options.py
@@ -5,20 +5,24 @@ from cat.looking_glass.white_rabbit import WhiteRabbit
 from cat.log import log
 
 
-@option("RabbitHole", priority=0)
+@option(RabbitHole, priority=0)
 class RabbitHoleDefault(RabbitHole):
-     def __init__(self, *args, **kwargs):
+    is_custom = True
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         log.warning("\n\n\nWELLAAA!!!!!!\n\n\n")
 
-@option("WhiteRabbit", priority=0)
+@option(WhiteRabbit, "WhiteRabbit", priority=0)
 class WhiteRabbitDefault(WhiteRabbit):
+    is_custom = True
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         log.warning("\n\n\nWELLAAA!!!!!!\n\n\n")
 
-@option("MainAgent", priority=0)
+@option(MainAgent, priority=0)
 class MainAgentDefault(MainAgent):
+    is_custom = True
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         log.warning("\n\n\nWELLAAA!!!!!!\n\n\n")
+
