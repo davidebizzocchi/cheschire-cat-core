3102872a563d5183aed65c14132aa256825a35fd
davidebizzocchi <davidebizzocchi19@gmail.com>
2025-01-06 00:29:40 +0100

    modified option decorator

--- core/cat/mad_hatter/decorators/options.py
+++ core/cat/mad_hatter/decorators/options.py
@@ -1,46 +1,41 @@
 from typing import Union, Callable, Type, Any
 
 class CatOption:
-    def __init__(self, name: str, class_: Type[Any], priority: int):
-        self.class_ = class_
+    def __init__(self, name: str, new_class_: Type[Any], old_class: Type, priority: int):
+        self.class_ = new_class_
+        self.old_class = old_class
         self.name = name
         self.priority = priority
 
     def __repr__(self) -> str:
-        return f"CatOption(name={self.name}, priority={self.priority})"
+        return f"CatOption(name={self.name}, old_class={self.old_class}, new_class={self.class_} priority={self.priority})"
 
-def option(*args: Union[str, Type[Any]], priority: int = 1) -> Union[CatOption, Callable]:
+def option(old_class: Type, *args: Union[str, Type[Any]], priority: int = 1) -> Union[CatOption, Callable]:
     """
     Make options out of classes, can be used with or without arguments.
+    old_class: The class to be replaced
+    
     Examples:
         .. code-block:: python
-            @option
+            @option(OldClass)
             class MyOption:
                 pass
             
-            @option("custom_name", priority=2)
+            @option(OldClass, priority=2)
             class MyOption:
                 pass
     """
 
-    def _make_with_name(option_name: Any) -> Callable:
-        if not isinstance(option_name, str):
-            option_name = option_name.__name__
+    def _make_with_name() -> Callable:
+        option_name = old_class.__name__
         def _make_option(class_: Type[Any]) -> CatOption:
-            option_ = CatOption(name=option_name, class_=class_, priority=priority)
+            option_ = CatOption(name=option_name, new_class_=class_, old_class=old_class, priority=priority)
             return option_
         return _make_option
+    
+    try:
+        old_class.__name__
+    except AttributeError:
+        raise ValueError("old_class must be a class")
 
-    if len(args) == 1 and isinstance(args[0], str):
-        # Example usage: @option("custom_name", priority=2)
-        return _make_with_name(args[0])
-    elif len(args) == 1 and isinstance(args[0], type):
-        # Example usage: @option
-        return _make_with_name(args[0].__name__)(args[0])
-    elif len(args) == 0:
-        # Example usage: @option(priority=2)
-        def _partial(class_: Type[Any]) -> CatOption:
-            return _make_with_name(class_.__name__)(class_)
-        return _partial
-    else:
-        raise ValueError("Too many arguments for option decorator")
+    return _make_with_name()
