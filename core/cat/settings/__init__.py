1a6d9ee388def5f2f824ec43312068606b96be08
davidebizzocchi <davidebizzocchi19@gmail.com>
2025-05-05 12:40:13 +0200

    settings blank init

--- core/cat/settings/__init__.py
+++
@@ -1,9 +0,0 @@
-from cat.settings.base import WonderlandSettings
-from cat.settings.redirect import setting_redirect
-
-cat_settings = WonderlandSettings()
-
-__all__ = [
-    "cat_settings",
-    "setting_redirect",
-]
